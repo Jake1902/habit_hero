d247dbbd264849d65f3fec8fd147374f88980b1e
Abel_Skaria2004 <123005471+Jake1902@users.noreply.github.com>
2025-07-15 19:04:40 +0530

    Add storage permission handling

--- lib/core/services/di.dart
+++ lib/core/services/di.dart
@@ -11,6 +11,7 @@ import 'settings_provider.dart';
 import 'template_service.dart';
 
 import 'notification_permission_service.dart';
+import 'storage_permission_service.dart';
 
 /// Registers app services in the provided [getIt] instance.
 void registerServices(GetIt getIt, SharedPreferences prefs) {
@@ -21,6 +22,8 @@ void registerServices(GetIt getIt, SharedPreferences prefs) {
 
   getIt.registerLazySingleton<NotificationPermissionService>(
       () => NotificationPermissionService(prefs));
+  getIt.registerLazySingleton<StoragePermissionService>(
+      () => StoragePermissionService());
 
   getIt.registerLazySingleton<AnalyticsService>(() => AnalyticsService(
       getIt<HabitRepository>(), getIt<CompletionRepository>()));
